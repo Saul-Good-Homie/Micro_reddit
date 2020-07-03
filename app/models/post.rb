02a3ffc86ab9bab3985803dc24461d4c35dab82b
Saul Belisle <rbelisle1270@gmail.com>
2020-07-03 15:20:48 -0700

    added comment on post feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true
 
     belongs_to :user, optional: true
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     validates :username, :email, presence: true, uniqueness: true
 
     has_many :posts
+    has_many :comments
 end
